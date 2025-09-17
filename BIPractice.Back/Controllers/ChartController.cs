7af5a22a06d36fe71936d3b983867fb45882f75d
Jazer <146768065+jazerQ@users.noreply.github.com>
2025-09-17 13:35:48 +0300

    написал метод для контроллера чтобы он из файла сохранял данные

--- BIPractice.Back/Controllers/ChartController.cs
+++ BIPractice.Back/Controllers/ChartController.cs
@@ -1,16 +1,20 @@
+using System.Text;
 using Application.Horizontal;
 using Application.Line;
 using Application.PieChart.GetCountOfAnyTypes;
 using Application.Scatter;
 using Application.Scatter.GetRatingAndCosts;
+using Aspose.Cells;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace BIPractice.Back.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class ChartController(IMediator mediator) : ControllerBase
+public class ChartController(IMediator mediator, IMongoDatabase db) : ControllerBase
 {
     [HttpGet("/pie")]
     public async Task<IActionResult> GetDataForPieChart()
@@ -88,6 +92,38 @@ public class ChartController(IMediator mediator) : ControllerBase
         {
             return BadRequest(ex.Message);
         }
+    }
+
+    [HttpPost("/upload-file")]
+    public async Task<IActionResult> UploadFile([FromForm] IFormFile file)
+    {
+        using (var stream = file.OpenReadStream())
+        {
+            var collection = db.GetCollection<BsonDocument>("BIObjects");
+            
+            var wb = new Workbook(stream, new TxtLoadOptions(LoadFormat.Csv)
+            {
+                Separator = ',',
+                Encoding = Encoding.UTF8,
+                HasFormula = false
+            });
+
+            var sheet = wb.Worksheets.FirstOrDefault() ?? throw new NullReferenceException("Данные не смог забрать");
+
+            for (int row = 1; row < sheet.Cells.MaxDataRow; row++)
+            {
+                var doc = new BsonDocument();
+                for (int col = 0; col < sheet.Cells.MaxDataColumn; col++)
+                {
+                    var header = sheet.Cells[0, col].Value.ToString();
+                    doc[header] = sheet.Cells[row, col].StringValue;
+                }
+                
+                await collection.InsertOneAsync(doc);
+            }
+        }
+
+        return Ok();
     }
     
 }
